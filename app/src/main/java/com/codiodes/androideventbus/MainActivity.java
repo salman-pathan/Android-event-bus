50e66ede45932e05a99c7bfa634f18ab1a2414ca
Salman Khan <salmanpathan30@gmail.com>
2016-05-02 19:11:44 +0530

    Added fragments into activity

--- app/src/main/java/com/codiodes/androideventbus/MainActivity.java
+++ app/src/main/java/com/codiodes/androideventbus/MainActivity.java
@@ -1,5 +1,6 @@
 package com.codiodes.androideventbus;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            TotalScoreFragment totalScoreFragment = new TotalScoreFragment();
+            IncrementScoreFragment incrementScoreFragment = new IncrementScoreFragment();
+
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.add(R.id.total_score_fragment_container, totalScoreFragment);
+            fragmentTransaction.add(R.id.increment_score_fragment_container, incrementScoreFragment);
+            fragmentTransaction.commit();
+        }
+
     }
 }
