b6371ae9456bfe5669d87e4f48f7e0741613ed2f
Salman Khan <salmanpathan30@gmail.com>
2016-05-02 18:17:30 +0530

    Initial Commit

---
+++ app/src/main/java/com/codiodes/androideventbus/IncrementScoreFragment.java
@@ -0,0 +1,29 @@
+package com.codiodes.androideventbus;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class IncrementScoreFragment extends Fragment {
+
+
+    public IncrementScoreFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_increment_score, container, false);
+    }
+
+}
